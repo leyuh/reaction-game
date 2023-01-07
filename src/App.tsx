091eae7e62992c56ce48d7a8489501ddbeadb183
Claire Devlin <clairebeargoldielox@icloud.com>
2023-01-07 09:08:24 -0800

    finish functionality, add score h1

--- src/App.tsx
+++ src/App.tsx
@@ -9,42 +9,106 @@ const App: React.FC = () => {
   const [sequence, setSequence] = useState<string[]>([]);
   const [plrSequence, setPlrSequence] = useState<string[]>([]);
   const [toggled, setToggled] = useState<string | null>(null);
+  const [lvl, setLvl] = useState<number>(1);
 
+  async function colorApp (color: string) {
+    let app: (HTMLElement | null) = document.getElementById("app")
+    if (app != null) {
+      
+      app.style.backgroundColor = color;
+      await interval(375);
+      app.style.backgroundColor = "white";
+    }
+  
+  }
+
+  const interval = (time: number) => {
+    return new Promise(r => setTimeout(r, time));
+  }
+
+  async function loopThroughSequence (seq: string[], newVal: string) {
+    await interval(1250);
+    for (let i = 0; i < seq.length; i++) {
+      setToggled(seq[i]);
+      await interval(375);
+      setToggled(null);
+      await interval(375);
+    }
+
+    setSequence((prev: string[]) => {
+      return [...prev, newVal];
+    })
+    setPlrsTurn(true);
+  }
+
+  // when player's turn is over, toggle next sequence
   useEffect(() => {
     if (!plrsTurn) {
       toggleSequence();
     }
   }, [plrsTurn])
 
+  // when plr sequence updates, check if it is equal to sequence. if so, next round
+  useEffect(() => {
+    if (plrSequence.length > 0) {
+      let equal: boolean = true;
+      if (plrSequence.length != sequence.length) {
+        equal = false;
+      }
+      if (equal) {
+        for (let i = 0; i < sequence.length; i++) {
+          if (sequence[i] != plrSequence[i]) {
+            equal = false;
+          }
+        }
+      }
+
+      if (equal) {
+        console.log("correct!");
+        setLvl((prev) => {
+          return prev + 1;
+        });
+        colorApp("green");
+        setPlrsTurn(false);
+      } else {
+        let equal: boolean = true;
+        for (let i = 0; i < plrSequence.length; i++) {
+          if (plrSequence[i] != sequence[i]) {
+            equal = false;
+          }
+        }
+        if (!equal) {
+          console.log("incorrect!");
+          setLvl(1);
+          colorApp("red");
+          setSequence([]);
+          setPlrsTurn(false);
+        }
+      }
+    }
+  }, [plrSequence])
+
   const toggleSequence = (): void => {
 
-    for (let i = 0; i < sequence.length; i++) {
-      setToggled(sequence[i]);
-      setTimeout(function() {
-        setToggled(null);
-      }, 1000)
-    }
+    setPlrSequence([]);
 
     let random = buttonColors[Math.floor(Math.random()*buttonColors.length)];
+    let tempSeq = [...sequence, random];
+    console.log(tempSeq);
 
-    setToggled(random);
-    setTimeout(function() {
-      setToggled(null);
-    }, 1000)
-
-    setSequence((prev: string[]) => {
-      return [...prev, random];
-    })
-    setPlrsTurn(true);
+    loopThroughSequence(tempSeq, random);
   }
 
   return (
     <div id="app">
+      <h1 id="score">{lvl}</h1>
       {buttonColors.map((val: string, i : number) => {
         return <Button
           color={val}
           plrsTurn={plrsTurn}
           toggled={toggled}
+          setToggled={setToggled}
+          setPlrSequence={setPlrSequence}
           key={i}/>
       })}
     </div>
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
-export const Button: React.FC<{ color : string, plrsTurn: boolean, toggled: string | null }> = ({color, plrsTurn, toggled}) => {
+export const Button: React.FC<{ color : string, plrsTurn: boolean, toggled: string | null, setToggled: Dispatch<SetStateAction<(string | null)>>, setPlrSequence: Dispatch<SetStateAction<(string[])>>}> = ({color, plrsTurn, toggled, setToggled, setPlrSequence}) => {
+
+    const interval = (time: number) => {
+        return new Promise(r => setTimeout(r, time));
+    }
+
+    async function unToggle () {
+        await interval(375);
+        setToggled(null);
+    }
 
     return (
-        <div className="button" style={{"backgroundColor": (toggled == color) ? color : ""}}>
+        <div className="button" style={{"backgroundColor": (toggled == color) ? color : ""}} onClick={() => {
+            if (plrsTurn) {
+                setToggled(color);
+                setPlrSequence((prev) => {
+                    return [...prev, color];
+                })
+                unToggle();
+            }
+        }}>
+            <div className="button-interior" style={{"backgroundColor": color}}></div>
         </div>
     );
 }
